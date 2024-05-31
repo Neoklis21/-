2006a2b9c59b7b9d336a96654dc9919ff5730c27
Neoklis21 <115157292+Neoklis21@users.noreply.github.com>
2024-05-31 15:18:32 +0300

    Update table_spesifications.py

--- table_spesifications.py
+++ table_spesifications.py
@@ -30,7 +30,7 @@ if uploaded_file is not None:
         
         
         st.sidebar.subheader('Scatter Plot')
-        features = data.columns[:-1]  # assuming the last column is the label
+        features = data.columns[:-1]  
         x_axis = st.sidebar.selectbox('Feature for X-axis', features)
         y_axis = st.sidebar.selectbox('Feature for Y-axis', features)
         

5f92c047723fbdacfbc1942cc1aa18fe8197ec3d
Neoklis21 <115157292+Neoklis21@users.noreply.github.com>
2024-05-31 12:49:43 +0300

    Create upload_data.py

---
+++ upload_data.py
@@ -0,0 +1,32 @@
+import streamlit as st
+import pandas as pd
+
+
+st.title('Data Analysis Application')
+
+
+st.sidebar.header('Upload your data')
+
+uploaded_file = st.sidebar.file_uploader("Choose a file", type=["csv", "xlsx"])
+
+if uploaded_file is not None:
+    try:
+       
+        if uploaded_file.name.endswith('.csv'):
+            data = pd.read_csv(uploaded_file)
+        # Load Excel files
+        elif uploaded_file.name.endswith('.xlsx'):
+            data = pd.read_excel(uploaded_file)
+        
+        st.write("Data successfully loaded!")
+        
+    
+        st.dataframe(data)
+        
+    except Exception as e:
+        st.error(f"Error: {e}")
+else:
+    st.info("Awaiting for file to be uploaded.")
+
+
+st.sidebar.info('This application allows you to upload CSV or Excel files and display the data.')
